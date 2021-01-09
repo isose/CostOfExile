02c4239fa0831ffabcae085dc0f21ca54667fe22
Andy Liu <ala158@sfu.ca>
2021-01-09 00:20:35 -0800

    Implemented backend endpoint to get characters  given account name

---
+++ server/routes/account.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const axios = require('axios');
+const parser = require('../scripts/parser');
+
+const router = express.Router();
+
+router.get('/characters', (req, res) => {
+  const accountName = req.body['account_name'];
+  const url = `https://www.pathofexile.com/character-window/get-characters?accountName=${accountName}`;
+  axios.get(`${url}`).then(
+    (characterList) => {
+      const processedList = parser.ParseCharacters(characterList.data);
+      res.json(processedList);
+    },
+    (error) => {
+      console.error(error);
+      // Status 422: Unprocessable Entity
+      res.sendStatus(422);
+    },
+  );
+});
+
+module.exports = router;
---
+++ server/scripts/parser.js
@@ -0,0 +1,23 @@
+function ParseCharacters(characterList) {
+  const processedList = [];
+
+  // eslint-disable-next-line no-restricted-syntax
+  for (const key in characterList) {
+    if (characterList.hasOwnProperty(key)) {
+      const characterData = characterList[key];
+      const processedData = {
+        name: characterData['name'],
+        league: characterData['league'],
+        level: characterData['level'],
+      };
+
+      processedList.push(processedData);
+    }
+  }
+
+  return processedList;
+}
+
+module.exports = {
+  ParseCharacters,
+};
