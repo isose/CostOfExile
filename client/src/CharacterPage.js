ee8fca957b89ade88b2cc72f6f486095f07a0585
wuandyw <wuandyw@sfu.ca>
2021-01-14 01:45:08 -0800

    Added Equip Boxes

--- client/src/CharacterPage.js
+++ client/src/CharacterPage.js
@@ -2,15 +2,26 @@ import React from 'react';
 import './CharacterPage.css';
 import Navbar from './Navbar.js';
 
-function ItemBox({ dimesion }) {
-  return <div></div>;
+function ItemBox(props) {
+  return <div className={props.className + ' unique-color'}></div>;
 }
 
 function EquipmentContainer() {
   return (
     <>
       <div className="div-equip">
-        <grid className="equipment-container"></grid>;
+        <grid className="equipment-container">
+          <ItemBox className="main-hand-weapon" />
+          <ItemBox className="off-hand-weapon" />
+          <ItemBox className="helmet" />
+          <ItemBox className="chestplate" />
+          <ItemBox className="belt" />
+          <ItemBox className="right-ring" />
+          <ItemBox className="left-ring" />
+          <ItemBox className="amulet" />
+          <ItemBox className="gloves" />
+          <ItemBox className="boots" />
+        </grid>
       </div>
     </>
   );
